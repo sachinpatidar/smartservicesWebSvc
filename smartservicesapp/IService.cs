525a4b5b958b0d5a9b74df6dd04774d9223eba19
Pawan Jatav <pawan.j@cisinlabs.com>
2016-10-13 13:24:42 +0530

    committed changes regarding Notification, Scrolling issues etc

---
+++ smartservicesapp/IService.cs
@@ -0,0 +1,26 @@
+﻿
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using smartservicesapp.Model;
+using System.Text;
+using System.Collections.Generic;
+
+namespace smartservicesapp
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface IService
+    {
+        [OperationContract]
+        [WebInvoke(Method = "GET", ResponseFormat = WebMessageFormat.Json, BodyStyle = WebMessageBodyStyle.Wrapped, UriTemplate = "GetCategoryList/{CategoryID}")]
+        List<Category> GetCategoryList(string CategoryID);
+
+        [OperationContract]
+        [WebInvoke(Method = "GET", ResponseFormat = WebMessageFormat.Json, BodyStyle = WebMessageBodyStyle.Wrapped, UriTemplate = "GetPrivacyTypeList/{PrivacyTypeID}")]
+        List<PrivacyType> GetPrivacyTypeList(string PrivacyTypeID);
+
+    }
+
+
+
+}
---
+++ smartservicesapp/Service.svc.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Activation;
+using smartservicesapp.Model;
+using System.Transactions;
+
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace smartservicesapp
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    [AspNetCompatibilityRequirements(RequirementsMode = AspNetCompatibilityRequirementsMode.Allowed)]
+    public class Service : IService
+    {
+        public List<Category> GetCategoryList(string CategoryID)
+        {
+            using (TransactionScope trans = new TransactionScope())
+            {
+                try
+                {
+                    RepsistoryEF<Category> _o = new global::RepsistoryEF<Model.Category>();
+                    int catID = 0;
+                    List<Category> lst = new List<Category>();
+                    if (!string.IsNullOrEmpty(CategoryID))
+                    {
+                        catID = int.Parse(CategoryID); lst = _o.GetListBySelector(z => z.CategoryID == catID).ToList();
+                    }
+                    else
+                    {
+                        lst = _o.GetList();
+                    }
+                    trans.Complete();
+                    return lst;
+                }
+                catch (Exception)
+                {
+                    trans.Dispose();
+                    throw;
+                }
+                finally
+                {
+                    trans.Dispose();
+                }
+            }
+
+        }
+        public List<PrivacyType> GetPrivacyTypeList(string PrivacyTypeID)
+        {
+            using (TransactionScope trans = new TransactionScope())
+            {
+                try
+                {
+                    RepsistoryEF<PrivacyType> _o = new global::RepsistoryEF<Model.PrivacyType>();
+                    int ID = 0;
+                    List<PrivacyType> lst = new List<PrivacyType>();
+                    if (!string.IsNullOrEmpty(PrivacyTypeID))
+                    {
+                        ID = int.Parse(PrivacyTypeID);
+                        lst = _o.GetListBySelector(z => z.PrivacyID == ID).ToList();
+                    }
+                    else
+                    {
+                        lst = _o.GetList();
+                    }
+                    trans.Complete();
+                    return lst;
+                }
+                catch (Exception)
+                {
+                    trans.Dispose();
+                    throw;
+                }
+                finally
+                {
+                    trans.Dispose();
+                }
+            }
+
+        }
+
+
+
+
+
+    }
+}
